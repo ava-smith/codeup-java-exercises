bc9f82527f99ae83492759ec46df008d752e224f
Ava Smith <ava013005@gmail.com>
2023-07-31 12:07:38 -0500

    finished exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+//        System.out.println("Hello, World!");
+//        int myFavoriteNumber = 21;
+//        System.out.println(myFavoriteNumber);
+        String myString = "3.14159";
+        System.out.println(myString);
+        long myNumber = 123;
     }
 }
