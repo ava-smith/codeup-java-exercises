e4a3adc18015778eba4d3143f297343fe7e12e07
Ava Smith <ava013005@gmail.com>
2023-08-07 12:07:04 -0500

    finished exercise wlakthrough

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -6,9 +6,11 @@ public class CircleApp {
 
     public static void main(String[] args) {
         Input input = new Input();
-        double radius = input.getDouble("Enter the decimal number or the radius of a circle.");
-        Circle myCircle = new Circle(radius);
+        do {
+            double radius = input.getDouble("Enter the decimal number or the radius of a circle.");
+            Circle myCircle = new Circle(radius);
 
-        System.out.printf("The area your circle  %.2f,  circumference is %.2f.%n", myCircle.getArea(), myCircle.getCircumference());
+            System.out.printf("The area your circle  %.2f,  circumference is %.2f.%n", myCircle.getArea(), myCircle.getCircumference());
+        } while(input.yesNo("Would you like to create another circle? [y/n]"));
     }
 }
