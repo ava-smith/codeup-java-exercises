b274be272b9c2944ed3b0ab19d1d779ecf5b3946
Ava Smith <ava013005@gmail.com>
2023-08-07 09:15:57 -0500

    finished input test

--- src/Person.java
+++ src/Person.java
@@ -2,21 +2,14 @@ public class Person {
 // 1.) Object Basics ====================
     private String name;
     public String getName(){
-//TODO: return the person's name
-        return name;
+        return this.name;
     }
 
     public void setName(String name){
-//TODO: change the name field to the passed value
         this.name = name;
     }
     public void sayHello(){
-//TODO: print a message to the console using the person's name
-        System.out.println("Hello, " + name);
-    }
-
-    public Person () {
-
+        System.out.printf("Hello, %s", this.name);
     }
 
     public Person (String name) {
---
+++ src/util/InputTest.java
@@ -0,0 +1,13 @@
+package util;
+
+public class InputTest {
+
+    public static void main(String[] args) {
+        Input userInput = new Input();
+//        System.out.println(userInput.getString());
+//        System.out.println(userInput.yesNo());
+//        System.out.println(userInput.getInt(1, 10));
+//        System.out.println(userInput.getInt());
+
+    }
+}
